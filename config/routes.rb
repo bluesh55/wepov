004052d36ae6916b1edfca402a58fba98cdc4a2d
cycorld <charleschoi87@gmail.com>
2015-10-29 20:58:28 +0900

    root to debates index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :points
   resources :debates
   devise_for :users
+
+  root 'debates#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
