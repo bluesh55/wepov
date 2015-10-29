cc8d2ec9805ca6e384001046f2b0c90b0793eb0e
cycorld <charleschoi87@gmail.com>
2015-10-29 20:55:29 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029115105) do
+ActiveRecord::Schema.define(version: 20151029115455) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "debate_id"
+    t.integer  "user_id"
+    t.text     "content"
+    t.integer  "comment_id"
+    t.integer  "like_count"
+    t.integer  "dislike_count"
+    t.boolean  "is_visible"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "comments", ["comment_id"], name: "index_comments_on_comment_id"
+  add_index "comments", ["debate_id"], name: "index_comments_on_debate_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "debates", force: :cascade do |t|
     t.integer  "user_id"
@@ -40,6 +56,21 @@ ActiveRecord::Schema.define(version: 20151029115105) do
 
   add_index "points", ["debate_id"], name: "index_points_on_debate_id"
   add_index "points", ["user_id"], name: "index_points_on_user_id"
+
+  create_table "reasons", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "point_id"
+    t.boolean  "is_pros"
+    t.string   "title"
+    t.text     "content"
+    t.integer  "priority"
+    t.boolean  "is_visible"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reasons", ["point_id"], name: "index_reasons_on_point_id"
+  add_index "reasons", ["user_id"], name: "index_reasons_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
