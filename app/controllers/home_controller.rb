33769894cf46a808b363cef25f3eba2888c86e87
cycorld <charleschoi87@gmail.com>
2015-10-30 02:50:34 +0900

    main debates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @debates = Debate.all
   end
 end
--- app/models/debate.rb
+++ app/models/debate.rb
@@ -1,3 +1,4 @@
 class Debate < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
 end
