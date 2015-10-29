b67787e4967aa51d0c74f8dd631886f5de809e80
cycorld <charleschoi87@gmail.com>
2015-10-29 23:12:07 +0900

    user valid test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :debates
+
+  validates :email, presence: true, length: {maximum: 255}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,21 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(email: "a@a.com", password: "12345678", password_confirmation: "12345678")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
