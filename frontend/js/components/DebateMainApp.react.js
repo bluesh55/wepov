15b7d5a22588112dd4448cd6ea6d3df1c6155964
seunghwan oh <seunghwan.oh5@gmail.com>
2015-11-02 16:53:10 +0900

    Banner 컴포넌트 생성

--- frontend/js/components/DebateMainApp.react.js
+++ frontend/js/components/DebateMainApp.react.js
@@ -4,6 +4,7 @@ var DebateActions = require('../actions/DebateActions');
 var DebateStore   = require('../stores/DebateStore');
 
 
+var Banner = require('./DebateMainApp/Banner.react');
 var Debates = require('./DebateMainApp/Debates.react');
 
 var getStateFromStores = function() {
@@ -31,28 +32,7 @@ var DebateMainApp = React.createClass({
   render: function() {
     return (
       <div id="app">
-        <div id="home" className="agency2-hero">
-          <div className="bg-overlay">
-            <div id="large-header" className="large-header">
-              <canvas id="demo-canvas" className="demo-canvas"></canvas>
-
-              <div className="hero-content-wrapper">
-                <div className="hero-content">
-                  <h1 className="hero-lead">Creative Agency</h1>
-                  <h4 className="h-alt hero-secondary">Web Design &amp; Development</h4>
-                  <a href="/debates/new" className="btn btn-light">새 논쟁 만들기</a>
-                  <a href="#about" className="scroller">
-                    <span className="scroller-text">scroll down</span>
-                    <span className="linea-basic-magic-mouse"></span>
-                  </a>
-
-                </div>
-              </div>
-
-            </div>
-
-          </div>
-        </div>
+        <Banner />
 
         <Debates
           debatesData={this.state.debatesData}
