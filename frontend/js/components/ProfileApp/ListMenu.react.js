9a7241e4bc68c3059e4a3478b4a0633949488473
seunghwan oh <seunghwan.oh5@gmail.com>
2015-11-03 19:26:12 +0900

    ListMenu 퍼블리싱

--- frontend/js/components/ProfileApp/ListMenu.react.js
+++ frontend/js/components/ProfileApp/ListMenu.react.js
@@ -4,7 +4,28 @@ var React = require('react');
 var ListMenu = React.createClass({
   render: function() {
     return (
-      <div>Menu</div>
+      <div id="ListMenu">
+        <ul className="list">
+          <li className="item">
+            <div className="image-wrapper">
+              <img src="#" />
+            </div>
+
+            <div className="title">
+              전체보기
+            </div>
+          </li>
+          <li className="item">
+            <div className="image-wrapper">
+              <img src="#" />
+            </div>
+
+            <div className="title">
+              전체보기
+            </div>
+          </li>
+        </ul>
+      </div>
     );
   }
 });
