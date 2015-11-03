9945ad61a592981a6cc7c49d8156465e24245758
seunghwan oh <seunghwan.oh5@gmail.com>
2015-11-03 13:13:53 +0900

    찬,반 데이터로 도넛그래프 출력

--- frontend/js/components/DebateShowApp/DebateBanner.react.js
+++ frontend/js/components/DebateShowApp/DebateBanner.react.js
@@ -1,6 +1,34 @@
 var React = require('react');
 
 var DebateBanner = React.createClass({
+  componentDidMount: function() {
+    var debate = this.props.debateData;
+
+    var data = [
+      {
+        value: debate.pros_count,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "찬성"
+      },
+      {
+        value: debate.cons_count,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "반대"
+      }
+    ];
+
+    var options = {
+    };
+
+    if(this.refs.chart) {
+      var ctx = this.refs.chart.getContext("2d");
+      var myDoughnutChart = new Chart(ctx).Doughnut(data,options);
+    }
+
+
+  },
   render: function() {
     var debateData = this.props.debateData;
     var imageURL = debateData.image.image.url;
@@ -9,6 +37,9 @@ var DebateBanner = React.createClass({
       backgroundImage: "url(" + imageURL + ")"
     } :
     null;
+
+    var isVoteEmpty = debateData.pros_count == 0 && debateData.cons_count == 0;
+
 
     return (
       <header className="page-title pt-light pt-plax-md-light"  style={style}>
@@ -24,22 +55,27 @@ var DebateBanner = React.createClass({
               </div>
 
               <div className="col-sm-6">
-                <div className="progress-box">
-                  <div className="progress">
-                    <div className="progress-bar" style={{width: '60%'}}></div>
-                  </div>
-                  <div className="progress-title pros">
-                    찬성 60%
-                  </div>
-                  <div className="progress-title cons">
-                    반대 40%
-                  </div>
-                </div>
+                {isVoteEmpty ? 
+                  <DebateBanner.EmptyVoteBox id="progressBox" /> :
+                  <canvas id="progressBox" ref="chart" />
+                }
               </div>
             </div>
           </div>
         </div>
       </header>
+    );
+  }
+});
+
+
+DebateBanner.EmptyVoteBox = React.createClass({
+
+  render: function() {
+    return (
+      <div id={this.props.id} className="empty">
+        가장 먼저 투표를 해보세요!
+      </div>
     );
   }
 });
