f025eddbde81316b97af2066eab0c0850b5f1568
seunghwan oh <seunghwan.oh5@gmail.com>
2015-11-05 18:31:25 +0900

    User Avatar Uploader 설정

--- db/migrate/20151029113557_devise_create_users.rb
+++ db/migrate/20151029113557_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :avatar, default: ""
       t.string :name, default: ""
       t.text   :intro
 
