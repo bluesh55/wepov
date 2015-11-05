bc221b41f66e411b20db09c9bbc2c26aa75b1975
seunghwan oh <seunghwan.oh5@gmail.com>
2015-11-05 14:24:07 +0900

    mypage에서 필요한 작성한 논논근 정보 json 리턴

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_votes = @user.vote_histories
-    @user_debates = @user.debates
-    @user_reasons = @user.reasons
+
+    respond_to do |format|
+      format.json do
+        debates_id = []
+        debates_id = @user.reasons.map { |r| r.point.debate.id }
+        debates_id += @user.points.map { |p| p.debate.id }
+        debates_id += @user.debates.map { |d| d.id }
+        debates_id = debates_id.uniq
+
+        @user_debates = Debate.where(id: debates_id)
+      end
+
+      format.html
+    end
   end
 end
---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,18 @@
+is_signed_in = !current_user.nil?
+
+
+json.debates @user_debates do |debate|
+  json.id debate.id
+  json.title debate.title
+  json.isMine is_signed_in && (debate.user_id == current_user.id)
+  json.points debate.points do |point|
+    json.id point.id
+    json.title point.title
+    json.isMine is_signed_in && (point.user_id == current_user.id)
+    json.reasons point.reasons do |reason|
+      json.id reason.id
+      json.title reason.title
+      json.isPros reason.is_pros
+    end
+  end
+end
