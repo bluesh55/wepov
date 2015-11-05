db41747b9eac1e755401f3d507d9b35e58a0a3e5
seunghwan oh <seunghwan.oh5@gmail.com>
2015-11-05 18:42:39 +0900

    유저 아바타기능 구현

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:name, :intro]
+    devise_parameter_sanitizer.for(:sign_up) << [:name, :intro, :avatar]
   end
 end
